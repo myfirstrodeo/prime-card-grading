812f99237a284f8213c8ce8d3f3a553d7d0fa031
myfirstrodeo <jrw5929@gmail.com>
2025-07-23 00:20:34 -0500

    Add files via upload

---
+++ src/Components/Contact.js
@@ -0,0 +1,11 @@
+
+import React from 'react';
+
+const Contact = () => (
+  <section id="contact">
+    <h2>Contact</h2>
+    <p>Content for Contact goes here.</p>
+  </section>
+);
+
+export default Contact;
---
+++ src/Components/SubmitForm.js
@@ -0,0 +1,11 @@
+
+import React from 'react';
+
+const SubmitForm = () => (
+  <section id="submitform">
+    <h2>SubmitForm</h2>
+    <p>Content for SubmitForm goes here.</p>
+  </section>
+);
+
+export default SubmitForm;

56e5baa5208497f5240287b3720f62d2a14e96cd
myfirstrodeo <jrw5929@gmail.com>
2025-07-22 23:25:04 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,1 +1,12 @@
+import React from 'react';
 
+function App() {
+  return (
+    <div>
+      <h1>Prime Card Grading</h1>
+      <p>This is a basic setup.</p>
+    </div>
+  );
+}
+
+export default App;

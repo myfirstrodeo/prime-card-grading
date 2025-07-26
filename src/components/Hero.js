422fe0f7a6b19d08526ff2ab24b8bb6bea137148
myfirstrodeo <jrw5929@gmail.com>
2025-07-26 09:57:30 -0400

    Add files via upload

---
+++ src/components/Hero.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Hero.css';
+import slabImage from '../assets/mj-slab.png';
+
+const Hero = () => {
+  return (
+    <section className="hero">
+      <div className="hero-content">
+        <h1>GRADING WITH<br />PRECISION.<br />BUILT FOR TRUST.</h1>
+        <button>Submit Your Card</button>
+      </div>
+      <img src={slabImage} alt="Graded Card Slab" />
+    </section>
+  );
+};
+
+export default Hero;

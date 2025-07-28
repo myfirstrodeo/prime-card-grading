39bd8067287a8410e8dfb79b30c70eef8dca820f
myfirstrodeo <jrw5929@gmail.com>
2025-07-28 07:50:23 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
+// src/App.js
 import React from 'react';
 import './App.css';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import PricingSection from './components/PricingSection';
+import logo from './assets/pcg.png'; // Make sure this path is correct
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Hero />
-      <PricingSection />
+    <div className="coming-soon-wrapper">
+      <img src={logo} alt="PCG Logo" className="logo" />
+      <h1>The Future of AI Card Grading</h1>
+      <p>Coming Soon</p>
     </div>
   );
 }

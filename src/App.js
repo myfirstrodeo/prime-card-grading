99f02e7e6bf414623ac31f10240442ed1fd11301
myfirstrodeo <jrw5929@gmail.com>
2025-08-04 23:21:36 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Prime Card Grading</title>
-  </head>
-  <body>
-    <div id="root"></div>
-  </body>
-</html>
+import React from "react";
+import "./App.css";
+import landingImage from "./src/graphic.png"; // make sure it's inside /src or /public
+
+function App() {
+  return (
+    <div style={{ backgroundColor: "#000", height: "100vh" }}>
+      <img
+        src={landingImage}
+        alt="Prime Card Grading"
+        style={{ width: "100%", height: "100%", objectFit: "contain" }}
+      />
+    </div>
+  );
+}
+
+export default App;

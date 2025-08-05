58a0e2df609cc2b6ca1ead78da3145153541a7e1
myfirstrodeo <jrw5929@gmail.com>
2025-08-04 23:31:19 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
-import "./App.css";
-import landingImage from "./graphic.png"; // or whatever image name you used
+import graphic from "./graphic.png"; // ✅ your uploaded image
 
 function App() {
   return (
-    <div style={{ backgroundColor: "#000", height: "100vh", display: "flex", alignItems: "center", justifyContent: "center" }}>
-      <img src={landingImage} alt="PCG Landing" style={{ width: "100%", maxWidth: "900px", height: "auto" }} />
+    <div style={{ backgroundColor: "#000", minHeight: "100vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
+      <img src={graphic} alt="PCG Home" style={{ maxWidth: "100%", height: "auto" }} />
     </div>
   );
 }
